071e5a0cac66aeaea1f34fd880399fb4ed78bbfa
irwanphan <irwanphan@gmail.com>
2023-08-27 23:54:39 +0700

    add idb and its methods

---
+++ src/scripts/data/favorite-restaurant-idb.js
@@ -0,0 +1,27 @@
+import { openDB } from 'idb';
+import CONFIG from '../globals/config';
+ 
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
+ 
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+  },
+});
+
+const FavoriteRestaurantIdb = {
+    async getFavorite(id) {
+        return (await dbPromise).get(OBJECT_STORE_NAME, id);
+    },
+    async getAllFavorites() {
+        return (await dbPromise).getAll(OBJECT_STORE_NAME);
+    },
+    async putFavorite(restaurant) {
+        return (await dbPromise).put(OBJECT_STORE_NAME, restaurant);
+    },
+    async deleteFavorite(id) {
+        return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+    },
+};
+
+export default FavoriteMovieIdb;
--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -3,7 +3,10 @@ const CONFIG = {
     BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images',
     DEFAULT_LANGUAGE: 'en-us',
     // CACHE_NAME: new Date().toISOString(),
-    CACHE_NAME: 'Restaurant-v1.0'
+    CACHE_NAME: 'Restaurant-v1.0',
+    DATABASE_NAME: 'restaurant-app-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'favorites',
   };
    
   export default CONFIG;
