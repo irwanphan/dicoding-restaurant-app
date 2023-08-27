ac47072effc8649056cd014c27d6666e69c52053
irwanphan <irwanphan@gmail.com>
2023-08-28 01:42:57 +0700

    add to favorite saved in indexedDB

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,6 +1,7 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantDbSource from '../../data/restaurantdb-source';
 import { templateRestaurantDetail } from '../templates/template';
+import FaveButtonInitiator from '../../utils/favorite-button-initiator';
  
 const Detail = {
   async render() {
@@ -15,6 +16,17 @@ const Detail = {
     // console.log(restaurant)
     const restaurantContainer = document.querySelector('#restaurant');
     restaurantContainer.innerHTML = templateRestaurantDetail(restaurant);
+
+    FaveButtonInitiator.init({
+      faveButtonContainer: document.querySelector('#faveButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        description: restaurant.description,
+        city: restaurant.city,
+        rating: restaurant.rating
+      }
+    });
   },
 };
  
