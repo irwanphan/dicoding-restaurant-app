ba0747d51461aed40038ae561ecc55aab6bdfbec
irwanphan <irwanphan@gmail.com>
2023-08-27 17:06:54 +0700

    add cache for offline usage

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,6 +2,8 @@ const CONFIG = {
     BASE_URL: 'https://restaurant-api.dicoding.dev',
     BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images',
     DEFAULT_LANGUAGE: 'en-us',
+    // CACHE_NAME: new Date().toISOString(),
+    CACHE_NAME: 'Restaurant-v1.0'
   };
    
   export default CONFIG;
