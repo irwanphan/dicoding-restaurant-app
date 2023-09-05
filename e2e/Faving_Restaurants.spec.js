a585d34298b04768060cb1e227f7b6bbf9b9b59e
irwanphan <irwanphan@gmail.com>
2023-09-05 17:51:08 +0700

    e2e: fav and unfav restaurant

--- e2e/Faving_Restaurants.spec.js
+++ e2e/Faving_Restaurants.spec.js
@@ -8,3 +8,43 @@ Scenario('showing empty favorited restaurants',  ({ I }) => {
     I.seeElement('#favedRestaurants');
     I.see('You have no favorite restaurant :(', '#favedRestaurants');
 });
+
+Scenario('favoriting one restaurant', async ({ I }) => {
+    I.see('You have no favorite restaurant :(', '#favedRestaurants');
+
+    I.amOnPage('/');
+
+    I.seeElement('.card a');
+    // pause();
+    I.click(locate('.card a').first());
+
+    I.seeElement('#faveButton');
+    I.click('#faveButton');
+
+    I.amOnPage('/#/favorite');
+    I.seeElement('.card');
+    // pause();
+});
+
+Scenario('favoriting then unfavoriting one restaurant', async ({ I }) => {
+    I.see('You have no favorite restaurant :(', '#favedRestaurants');
+    I.amOnPage('/');
+
+    I.seeElement('.card a');
+    I.click(locate('.card a').first());
+    
+    I.seeElement('#faveButton');
+    I.click('#faveButton');
+    
+    I.amOnPage('/#/favorite');
+    I.seeElement('.card');
+    // pause();
+
+    I.click(locate('.card a').first());
+
+    I.seeElement('#faveButton');
+    I.click('#faveButton');
+    I.amOnPage('/#/favorite');
+    I.see('You have no favorite restaurant :(', '#favedRestaurants');
+    // pause();
+});
