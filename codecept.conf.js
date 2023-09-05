5174c0ff0a254ad0e2ca2bfdb90cc8d20e3e4364
irwanphan <irwanphan@gmail.com>
2023-09-05 16:50:41 +0700

    codecept ssOnFail false

--- codecept.conf.js
+++ codecept.conf.js
@@ -20,5 +20,10 @@ exports.config = {
   include: {
     I: './steps_file.js'
   },
-  name: 'dicoding-restaurant-app'
+  name: 'dicoding-restaurant-app',
+  plugins: {
+    screenshotOnFail: {
+      enabled: false,
+    },
+  },
 }
