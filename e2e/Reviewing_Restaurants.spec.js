567ad38a811bf059a4ce01d3d84a460a6a028bae
irwanphan <irwanphan@gmail.com>
2023-09-09 14:11:27 +0700

    e2e: focus when missing value

--- e2e/Reviewing_Restaurants.spec.js
+++ e2e/Reviewing_Restaurants.spec.js
@@ -4,13 +4,32 @@ Before(({ I }) => {
     I.amOnPage('/');
 });
 
+Scenario('add review but missing name and review, then focus on it', async ({ I }) => {
+    I.seeElement('.card a');
+    I.click(locate('.card a').first());
+
+    I.seeElement('.review-form button');
+    I.click('.review-form button');
+
+    I.seeElement('.review-form input[type=text]');
+    const inputName = await I.grabValueFrom('.review-form input[type=text]');
+    if (inputName == '') {
+        I.focus('.review-form input[type=text]');
+    }
+    I.fillField('.review-form input[type=text]', 'John Doe');
+
+    I.seeElement('.review-form textarea');
+    const inputReview = await I.grabValueFrom('.review-form textarea');
+    if (inputReview == '') {
+        I.focus('.review-form textarea');
+    }
+});
+
 Scenario('add review to one restaurant', async ({ I }) => {
     I.seeElement('.card a');
-    // pause();
     I.click(locate('.card a').first());
     
     I.seeElement('.review-form input[type=text]');
-    // pause();
     I.fillField('.review-form input[type=text]', 'John Doe');
     I.seeElement('.review-form textarea');
     I.fillField('.review-form textarea', 'Ga pakai Lorem Ipsum buat review test e2e');
