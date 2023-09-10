5205b5fa1745005052332931abe9e149d0b6b9fa
irwanphan <irwanphan@gmail.com>
2023-09-10 13:19:11 +0700

    .

--- sharp.js
+++ sharp.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
  
 const target = path.resolve(__dirname, 'src/public/images');
-const destination = path.resolve(__dirname, 'public/images');
+const destination = path.resolve(__dirname, 'public');
  
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
